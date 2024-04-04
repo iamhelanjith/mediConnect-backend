3f81be959102bdcc62670f8c0bc8081154932d09
helanjith <helanm06@gmail.com>
2024-04-04 09:24:14 +0530

    commit

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const dbConfig = async () => {
+
+  const MONGO_URL = "mongodb+srv://user:user@cluster0.cygeykj.mongodb.net/MediConnectDB?retryWrites=true&w=majority&appName=Cluster0"
   try {
-    await mongoose.connect(process.env.MONGO_URL, {});
+    await mongoose.connect(MONGO_URL, {});
     console.log("Connected to the mongoDB database");
   } catch (error) {
     console.log("Error connecting to the database", error);
